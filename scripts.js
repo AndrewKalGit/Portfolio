63f7bdb1d03c186c32d9d62ebe23fe464033ed85
Andrew Kal <andrewkalgit@gmail.com>
2022-02-24 16:28:00 -0500

    mvp

---
+++ scripts.js
@@ -0,0 +1,21 @@
+function scrollWin2() {
+    const element = document.getElementById("pdf_anchor");
+    element.scrollIntoView();
+}
+
+function scrollWin3() {
+    const element = document.getElementById("flex-skills");
+    element.scrollIntoView();
+}
+
+function scrollWin4() {
+    const element = document.getElementById("contact");
+    element.scrollIntoView();
+}
+
+function myFunction() {
+    const element = document.body;
+    element.classList.toggle("dark-mode");
+ }
+
+
